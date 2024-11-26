90109ee9f093c54d53fdfb274b5f7a59e6f50899
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2024-11-26 11:24:39 +0530

    worked on playwright in qa

--- tests/variable.ts
+++ tests/variable.ts
@@ -3,6 +3,5 @@ const Password = "$Mp";
 const code = "c100001";
 const username = "$user";
 const userPassword = "$Up";
-const value = "rrr";
 
 export { id, Password, code, username, userPassword };
