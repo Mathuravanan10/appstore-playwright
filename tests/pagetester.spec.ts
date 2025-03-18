9393fe3c0ce58c30dedc2fa8037a64020b52072e
Mathuravanan10 <mathuravanan.s@basiscloudsolutions.com>
2025-03-18 16:14:25 +0530

    worked sap user craetion

--- tests/pagetester.spec.ts
+++ tests/pagetester.spec.ts
@@ -4,9 +4,9 @@ import { productMainPageList, productMainHeaderList, productFooterList, menuPage
 import { testingCardPage } from "../webPages/TownHouseCardList";
 import { testingHeaderPage } from "../webPages/TownHouseHeader";
 import { kellanova } from "../webPages/kellanovaHomePage";
-import { kellanovabody, kellanovamainpage, kellanvaImgs, productDetails } from "../variableFiles/kellanova";
-import { cheezitDropdown, cheezitDropdownShop, cheezitFooter, cheezitMainpage, cheezitProductPage, FooterLinks, HeaderWhereToBuy, headingpage, kellanovaHeaderLinks, searchpage, whereToBuyRegionInputData, whereToBuySoldByInputData, whereToBuyTellMeAboutInputData } from "../variableFiles/kellanovaSimple";
-import { Testpage } from "../webPages/testpage";
+// import { kellanovabody, kellanovamainpage, kellanvaImgs, productDetails } from "../variableFiles/kellanova";
+// import { cheezitDropdown, cheezitDropdownShop, cheezitFooter, cheezitMainpage, cheezitProductPage, FooterLinks, HeaderWhereToBuy, headingpage, kellanovaHeaderLinks, searchpage, whereToBuyRegionInputData, whereToBuySoldByInputData, whereToBuyTellMeAboutInputData } from "../variableFiles/kellanovaSimple";
+// import { Testpage } from "../webPages/testpage";
 
 let page: any;
 let mainPage: testingPage;
@@ -31,35 +31,35 @@ test.describe('multiple checks using steps', async () => {
   // await mainPage.MainPageFooter(productFooterList, locator);
   });
 
-  // await test.step("Card List Test", async () => {
-  //   test.setTimeout(400000);
-  //   const locator = 'https://www.townhousecrackers.ca/en_CA/home.html';
-  //   await page.goto(locator);
-  //   cardlist = new testingCardPage(page);
-  //   await cardlist.testingCardList(productMainHeaderList, locator);
-  //   await cardlist.cardListButton(cardListButton, locator)
-  // });
+  test("Card List Test", async () => {
+    test.setTimeout(400000);
+    const locator = 'https://www.townhousecrackers.ca/en_CA/home.html';
+    await page.goto(locator);
+    cardlist = new testingCardPage(page);
+    await cardlist.testingCardList(productMainHeaderList, locator);
+    await cardlist.cardListButton(cardListButton, locator)
+  });
 
-  // await test.step("Header Page Test", async () => {
-  //   test.setTimeout(500000);
-  //   const locator = 'https://www.townhousecrackers.ca/en_CA/home.html';
-  //   const ourFood = 'https://www.townhousecrackers.ca/en_CA/products.html';
-  //   const ourFoodLink = 'https://www.townhousecrackers.ca/en_CA/products/Kellogg-s-Town-House-FlipSides-Original-Crackers-product.html';
-  //   await page.goto(locator);
-  //   cardHeaderlist = new testingHeaderPage(page);
-  //   cardlist = new testingCardPage(page);
-  //   await cardHeaderlist.testingHeaderList(menuPageOurFood, ourFood, ourFoodLink);
-  //   await cardHeaderlist.testingHeaderRecipesList(cardlist, locator);
-  //   await cardHeaderlist.testingHeaderSignUpList(locator, appCreate);
-  //   await cardHeaderlist.testingHeaderWhereToByList(buyRegionButton, buySoldByButton);
-  // });
+  test("Header Page Test", async () => {
+    test.setTimeout(500000);
+    const locator = 'https://www.townhousecrackers.ca/en_CA/home.html';
+    const ourFood = 'https://www.townhousecrackers.ca/en_CA/products.html';
+    const ourFoodLink = 'https://www.townhousecrackers.ca/en_CA/products/Kellogg-s-Town-House-FlipSides-Original-Crackers-product.html';
+    await page.goto(locator);
+    cardHeaderlist = new testingHeaderPage(page);
+    cardlist = new testingCardPage(page);
+    await cardHeaderlist.testingHeaderList(menuPageOurFood, ourFood, ourFoodLink);
+    await cardHeaderlist.testingHeaderRecipesList(cardlist, locator);
+    await cardHeaderlist.testingHeaderSignUpList(locator, appCreate);
+    await cardHeaderlist.testingHeaderWhereToByList(buyRegionButton, buySoldByButton);
+  });
 
-  // await test.step("Footer Test", async () => {
-  //   test.setTimeout(600000);
-  //   const locator = 'https://www.townhousecrackers.ca/en_CA/home.html';
-  //   await page.goto(locator);
-  //   await mainPage.MainPageFooter(productFooterList, locator);
-  // });
+  test("Footer Test", async () => {
+    test.setTimeout(600000);
+    const locator = 'https://www.townhousecrackers.ca/en_CA/home.html';
+    await page.goto(locator);
+    await mainPage.MainPageFooter(productFooterList, locator);
+  });
 
   // test("Kellanova Mainpage Test", async () => {
   //   const locator = 'https://www.kellanova.ca/en_CA/home.html';
@@ -71,17 +71,17 @@ test.describe('multiple checks using steps', async () => {
   //   await mainPage.SearchTesting()
   // });
 
-  test("Kellanova Home page", async () => {
-    test.setTimeout(600000);
-    kellano = new kellanova(page);
-    const locator = 'https://www.kellanovaus.com/us/en/home.html';
-    await page.goto(locator);
-    const testpage = new Testpage(page);
-    // await kellano.kellanovaHomePage(kellanovamainpage, locator);
-    // await kellano.kellanovaHomePageImgs(kellanvaImgs, locator, kellanovabody);
-    // await kellano.kellanovaHeadingPart(testpage, productDetails);
-    await kellano.sapI5();
-  });
+  // test("Kellanova Home page", async () => {
+  //   test.setTimeout(600000);
+  //   kellano = new kellanova(page);
+  //   const locator = 'https://www.kellanovaus.com/us/en/home.html';
+  //   await page.goto(locator);
+  //   const testpage = new Testpage(page);
+  //   // await kellano.kellanovaHomePage(kellanovamainpage, locator);
+  //   // await kellano.kellanovaHomePageImgs(kellanvaImgs, locator, kellanovabody);
+  //   // await kellano.kellanovaHeadingPart(testpage, productDetails);
+  //   await kellano.sapI5();
+  // });
 
   // test("Cheezit Home page", async () => {
   //   kellano = new kellanova(page);
